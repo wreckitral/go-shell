9a82c019e31afafd9d189ec8449567316dc48f55
Defhanaya Sofhiea <defhanayasofhiea@gmail.com>
2024-06-18 16:57:19 +0700

    feat: add the exit builtin

--- cmd/goshell/main.go
+++ cmd/goshell/main.go
@@ -10,7 +10,12 @@ import (
 func main() {
     for {
         fmt.Fprint(os.Stdout, "$ ")
+
         command, _ := bufio.NewReader(os.Stdin).ReadString('\n')
+        if command == "exit 0\n" {
+            os.Exit(0)
+        }
+
         fmt.Printf("%s: command not found\n", strings.TrimRight(command, "\n"))
     }
 }
